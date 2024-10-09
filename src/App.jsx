88f1b08d6e01e9c124a19bff2709223dc0807a2c
Andrii Zabolotin <fenics7511@gmail.com>
2024-10-09 10:47:25 +0300

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Routes, Route} from 'react-router-dom'
+import StaffLayout from "./components/StaffLayout.jsx";
+import Home from "./pages/HomePage.jsx";
+import UserLayout from "./components/UserLayout.jsx";
+import Profile from "./pages/ProfilePage.jsx";
+import Admin from "./pages/AdminPage.jsx";
+import NotFound from "./pages/NotFoundPage.jsx";
+import Attendances from "./pages/AttendancesPage.jsx";
+import Users from "./pages/UsersPage.jsx";
+import Trainings from "./pages/TrainingsPage.jsx";
+import Subscriptions from "./pages/SubscriptionsPage.jsx";
+import PermissionGroups from "./pages/PermissionGroupsPage.jsx";
+import Login from "./pages/LoginPage.jsx";
+import React from "react";
+import RequireAuth from "./hoc/RequireAuth.jsx";
+import { AuthProvider } from './hoc/AuthProvider.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <AuthProvider>
+        <Routes>
+            <Route path="/" element={<UserLayout />}>
+                <Route index element={<Home />} />
+                <Route path="login" element={<Login />} />
+            </Route>
+            <Route path="/admin" element={<StaffLayout/>}>
+                <Route index element={
+                    <RequireAuth>
+                        <Admin/>
+                    </RequireAuth>
+                }/>
+                <Route path="attendances" element={
+                    <RequireAuth>
+                        <Attendances/>
+                    </RequireAuth>
+                }/>
+                <Route path="users" element={
+                    <RequireAuth>
+                        <Users/>
+                    </RequireAuth>
+                }/>
+                <Route path="trainings" element={
+                    <RequireAuth>
+                        <Trainings/>
+                    </RequireAuth>
+                }/>
+                <Route path="subscriptions" element={
+                    <RequireAuth>
+                        <Subscriptions/>
+                    </RequireAuth>
+                }/>
+                <Route path="permission-groups" element={
+                    <RequireAuth>
+                        <PermissionGroups/>
+                    </RequireAuth>
+                }/>
+                <Route path="profile" element={
+                    <RequireAuth>
+                        <Profile/>
+                    </RequireAuth>
+                }/>
+            </Route>
+            <Route path="*" element={<NotFound/>}/>
+        </Routes>
+    </AuthProvider>
     </>
   )
 }
