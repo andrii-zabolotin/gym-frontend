822a8b1f961789cf39de367900fe15c414497f32
Andrii Zabolotin <fenics7511@gmail.com>
2024-10-12 09:21:57 +0300

    Create AdminProfile page

---
+++ src/pages/AdminProfilePage.jsx
@@ -0,0 +1,18 @@
+import { useAuth } from '../hook/useAuth'
+import { useNavigate } from 'react-router-dom';
+
+export default function AdminProfile() {
+    const {logout} = useAuth();
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <h1>PROFILE</h1>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded" onClick={() => {
+                logout(
+                    () => navigate('/', {replace: true})
+                )
+            }}>Logout</button>
+        </>
+    )
+}
