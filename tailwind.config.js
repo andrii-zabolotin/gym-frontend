555444f592ed20ea844b110d713b8bdbe8f6e119
Andrii Zabolotin <fenics7511@gmail.com>
2024-10-12 09:18:29 +0300

    Install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx}",],
+  theme: {
+    extend: {
+      colors: {
+        "gray-90": "#E6E6E6"
+      }
+    },
+  },
+  plugins: [],
+}
+
