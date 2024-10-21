27de122c0ea151be5fe5a087c9f87835e8ee8c64
Andrii Zabolotin <fenics7511@gmail.com>
2024-10-21 11:32:10 +0300

    Change Field component

--- src/components/Inputs/Field.jsx
+++ src/components/Inputs/Field.jsx
@@ -1,11 +1,15 @@
-export default function Field({placeholder, value, onChange}) {
+export default function Field({placeholder, value, disabled, errors= {}, ...props}) {
     return (
         <input
             type="text"
             placeholder={placeholder}
-            value={value}
-            onChange={onChange}
-            className="appearance-none leading-tight focus:outline-none border border-gray-300 rounded-3xl p-2 px-5"
+            value={value || ""}
+            disabled={disabled}
+            className={`appearance-none focus:outline-none leading-tight border rounded-2xl p-2 px-5 
+                ${disabled ? 'text-gray-400 bg-gray-50 border-gray-300' : ''}
+                ${errors[props.id] ? 'border border-red-500 text-red-500' : ''}`
+        }
+            {...props}
         />
     )
 }
