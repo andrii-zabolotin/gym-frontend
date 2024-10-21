23a7ce24216fa836197856d7a77112077b60d892
Andrii Zabolotin <fenics7511@gmail.com>
2024-10-21 11:31:32 +0300

    Create CustomDate component

---
+++ src/components/Inputs/CustomDate.jsx
@@ -0,0 +1,13 @@
+export default function CustomDate({value, onChange, disabled, ...props}) {
+    return (
+        <input
+            type="date"
+            value={value || ""}
+            onChange={onChange}
+            disabled={disabled}
+            className={`appearance-none focus:outline-none leading-tight border rounded-2xl p-2 px-5 
+                ${disabled ? 'text-gray-400 bg-gray-50 border-gray-300' : ''}`}
+            {...props}
+        />
+    )
+}
