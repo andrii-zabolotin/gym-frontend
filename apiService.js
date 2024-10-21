34286b6c4bc4a9ab874aa53427ca2f052a09d290
Andrii Zabolotin <fenics7511@gmail.com>
2024-10-21 11:30:37 +0300

    Create new api endpoints

--- apiService.js
+++ apiService.js
@@ -1,11 +1,34 @@
 import api from "./axios.config.js";
 
-export const getAttendances = (data) => {
+export const getAttendances = ( data, userId) => {
+    if (userId) return api.get(`attendances/?user_id=${userId}`, data)
     return api.get("attendances/", data)
+};
+
+export const getTrainings = ( data ) => {
+    return api.get("trainings/", data)
+}
+
+export const getParticipants = ( trainingId ) => {
+    return api.get(`trainings-users/?training_id=${trainingId}`)
+}
+
+export const getUserTrainings = (data, userId) => {
+    if (userId) return api.get(`trainings-users/?user_id=${userId}`, data)
+    return api.get("trainings-users/", data)
+};
+
+export const getUserSubscriptions = (data, userId) => {
+    if (userId) return api.get(`subscriptions-users/?user_id=${userId}`, data)
+    return api.get("subscriptions-users/", data)
 };
 
 export const getUser = (data) => {
     return api.get("user/list/", data)
+};
+
+export const patchUser = (id, data) => {
+  return api.patch(`user/${id}/`, data)
 };
 
 export const getTrainingTypes = () => {
@@ -23,7 +46,7 @@ export const getTrainers = () => {
 export const getMassagist = () => {
     return api.get("user/staff/", {
         params: {
-            group: "Масажист"
+            group: "Масажисти"
         }
     })
 };
